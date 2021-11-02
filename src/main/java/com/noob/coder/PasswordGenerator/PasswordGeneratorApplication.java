fedcb05caf1800baf02ea7588a5cb936f4f76848
WilcyWilson <wilsonshresthaapps@gmail.com>
2021-11-03 03:05:57 +0545

    Project Startup

---
+++ src/main/java/com/noob/coder/PasswordGenerator/PasswordGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.noob.coder.PasswordGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noob/coder/PasswordGenerator/PasswordGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noob.coder.PasswordGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
