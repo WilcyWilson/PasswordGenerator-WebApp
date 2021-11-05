d9c3fd2cab757e923e0af210e8262361b573af6f
WilcyWilson <wilsonshresthaapps@gmail.com>
2021-11-05 18:20:57 +0545

    bootstrap,js,thymeleaf

--- src/main/java/com/noob/coder/PasswordGenerator/web/HomeController.java
+++ src/main/java/com/noob/coder/PasswordGenerator/web/HomeController.java
@@ -1,14 +1,24 @@
 package com.noob.coder.PasswordGenerator.web;
 
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.noob.coder.PasswordGenerator.service.PasswordGeneratorService;
+
 @Controller
 public class HomeController {
+
+	@Autowired
+	private PasswordGeneratorService passwordService;
+
 	@GetMapping("/")
-	public String getHomeView(ModelMap model) {
-		model.put("name","Wilson Shrestha");
+	public String getHomeView(ModelMap model) throws NoSuchAlgorithmException {
+		String password = passwordService.firstAlgorithm();
+		model.put("password", password);
 		return "index";
 	}
 }
