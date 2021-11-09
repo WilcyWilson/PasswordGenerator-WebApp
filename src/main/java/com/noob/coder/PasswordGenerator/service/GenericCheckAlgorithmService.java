f93fe51a4e9e97bbbb2042cdb1350cc1f0e3a05a
WilcyWilson <wilsonshresthaapps@gmail.com>
2021-11-09 11:49:43 +0545

    DI using Autowired

--- src/main/java/com/noob/coder/PasswordGenerator/service/GenericCheckAlgorithmService.java
+++ src/main/java/com/noob/coder/PasswordGenerator/service/GenericCheckAlgorithmService.java
@@ -25,6 +25,7 @@ public class GenericCheckAlgorithmService implements ICheckAlgorithmService {
 		List<String> symbols = new ArrayList<String>(
 				Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h",
 						"i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
+
 		if (Boolean.TRUE.equals(homeEntity.getSymbols())) {
 			List<String> newSymbols = new ArrayList<String>(Arrays.asList("@", "#", "$", "%"));
 			symbols.addAll(newSymbols);
