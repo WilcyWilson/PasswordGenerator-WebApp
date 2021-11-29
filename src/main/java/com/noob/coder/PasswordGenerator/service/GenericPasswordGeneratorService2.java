a86ae2913d20a14f97b34973ce5955786780f684
wilcywilson <wilsonshresthaapps@gmail.com>
2021-11-29 14:20:38 +0545

    multiple @Service which?

---
+++ src/main/java/com/noob/coder/PasswordGenerator/service/GenericPasswordGeneratorService2.java
@@ -0,0 +1,17 @@
+package com.noob.coder.PasswordGenerator.service;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenericPasswordGeneratorService2 implements IPasswordGeneratorService{
+
+	@Override
+	public String algorithm(List<String> symbols, int length) throws NoSuchAlgorithmException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
