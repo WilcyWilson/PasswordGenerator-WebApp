326025dca9cf2fa2f6cb34966f17964f13919054
WilcyWilson <wilsonshresthaapps@gmail.com>
2022-04-30 18:06:10 +0545

    lombok

--- src/main/java/com/noob/coder/PasswordGenerator/service/ICheckAlgorithmService.java
+++ src/main/java/com/noob/coder/PasswordGenerator/service/ICheckAlgorithmService.java
@@ -5,5 +5,5 @@ import java.security.NoSuchAlgorithmException;
 import com.noob.coder.PasswordGenerator.entity.HomeEntity;
 
 public interface ICheckAlgorithmService {
-	public String checkAlgorithm(HomeEntity homeEntity) throws NoSuchAlgorithmException;
+	String checkAlgorithm(HomeEntity homeEntity) throws NoSuchAlgorithmException;
 }
