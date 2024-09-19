a2e29a4802234a035b0a4883e362905cf5083b70
wilcywilson <wilsonshresthaapps@gmail.com>
2024-09-20 05:21:55 +0545

    Password Length limit and favicon

--- src/main/java/com/noob/coder/PasswordGenerator/service/GenericCheckAlgorithmService.java
+++ src/main/java/com/noob/coder/PasswordGenerator/service/GenericCheckAlgorithmService.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +13,7 @@ import com.noob.coder.PasswordGenerator.entity.HomeEntity;
 @Service
 public class GenericCheckAlgorithmService implements ICheckAlgorithmService {
 	private final IPasswordGeneratorService passwordService;
+	private final static int lengthLimit = 32;
 
 	public GenericCheckAlgorithmService(@Qualifier("genericPasswordGeneratorService") IPasswordGeneratorService passwordService) {
 		this.passwordService = passwordService;
@@ -21,7 +21,7 @@ public class GenericCheckAlgorithmService implements ICheckAlgorithmService {
 
 	@Override
 	public String checkAlgorithm(HomeEntity homeEntity) throws NoSuchAlgorithmException {
-		int length = homeEntity.getLength();
+		int length = Math.min(homeEntity.getLength(), lengthLimit);
 		List<String> symbols = new ArrayList<String>(
 				Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h",
 						"i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
